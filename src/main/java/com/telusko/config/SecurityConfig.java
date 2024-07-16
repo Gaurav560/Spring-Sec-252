a185f4c8afaada3c56d2b60fd35174a8f490e6c4
GAURAV SHARMA <gsharma1154@gmail.com>
2024-07-16 23:05:31 +0530

    disabling csrf

--- src/main/java/com/telusko/config/SecurityConfig.java
+++ src/main/java/com/telusko/config/SecurityConfig.java
@@ -15,14 +15,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).
-                authorizeHttpRequests(authorizeRequests ->
+        http.csrf(AbstractHttpConfigurer::disable);
+                http.authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/").permitAll()
                                 .anyRequest().authenticated()
-                )
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+                );
+               http.httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
